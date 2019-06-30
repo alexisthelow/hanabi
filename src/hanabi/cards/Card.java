83428d830a625c7b1aed0a68cb2f524b4605f4f5
Alexis Low <alexisthelow@gmail.com>
2019-06-29 18:39:19 -0600

    todos and a little cleanup

--- src/hanabi/cards/Card.java
+++ src/hanabi/cards/Card.java
@@ -1,25 +1,9 @@
 package hanabi.cards;
 
-import javax.swing.JTable;
-
 public class Card {
 	
 	private Color color;
 	
 	private Suit suit;
-	
-	private String[] columnNames = {"Blue", "Green", "Red", "White", "Yellow", "Multicolor"};
-	
-	private ThreeState[][] rowData = {
-			{ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE}, 
-			{ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE}, 
-			{ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE}, 
-			{ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE}, 
-			{ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE}, 
-	};
-	
-	private JTable cardInfo = new JTable(rowData, columnNames);
-	
-	
 
 }
--- src/hanabi/game/AbstractPlayer.java
+++ src/hanabi/game/AbstractPlayer.java
@@ -1,5 +1,7 @@
 package hanabi.game;
 
+import java.util.ArrayList;
+
 import javax.swing.JTable;
 
 import hanabi.cards.Hand;
@@ -8,8 +10,14 @@ import hanabi.cards.ThreeState;
 public class AbstractPlayer {
 	
 	private Hand hand;
+	private ArrayList<JTable> cardInfoTables = new ArrayList<JTable>();
 	
 	
+	//TODO receive and interpret info
+	//TODO deduce from visible cards
+	//TODO discard
+	//TODO play card
+	//TODO draw card
 	
 	public JTable getNewCardInfoTable() {
 		
