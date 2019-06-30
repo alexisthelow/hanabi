89469403336f9f2973ef643056f9120f04cbc610
Alexis Low <alexisthelow@gmail.com>
2019-06-29 18:20:28 -0600

    hand states n behaviors

--- src/hanabi/cards/Hand.java
+++ src/hanabi/cards/Hand.java
@@ -5,6 +5,14 @@ import java.util.ArrayList;
 public class Hand {
 	
 	private ArrayList<Card> cards = new ArrayList<Card>();
-	
+
+	public Hand(ArrayList<Card> cards) {
+		super();
+		this.cards = cards;
+	}
+
+	public ArrayList<Card> getCards() {
+		return cards;
+	}
 
 }
