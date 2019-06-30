a4232f683c1f03c25d17fc6cb181df918bfc71c4
Alexis Low <alexisthelow@gmail.com>
2019-06-29 18:27:26 -0600

    moved cardinfotable to player, where it should've been in the first place

--- src/hanabi/game/AbstractPlayer.java
+++ src/hanabi/game/AbstractPlayer.java
@@ -1,5 +1,28 @@
 package hanabi.game;
 
+import javax.swing.JTable;
+
+import hanabi.cards.Hand;
+import hanabi.cards.ThreeState;
+
 public class AbstractPlayer {
+	
+	private Hand hand;
+	
+	
+	
+	private String[] columnNames = {"Blue", "Green", "Red", "White", "Yellow", "Multicolor"};
+	
+	private ThreeState[][] rowData = {
+			{ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE}, 
+			{ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE}, 
+			{ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE}, 
+			{ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE}, 
+			{ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE, ThreeState.MAYBE}, 
+	};
+	
+	private JTable cardInfo = new JTable(rowData, columnNames);
+	
+	
 
 }
