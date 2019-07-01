34396b56672e19b447187cec689a66b0223aa7d7
Alexis Low <alexisthelow@gmail.com>
2019-07-01 15:33:13 -0600

    updated global card tracker

--- src/hanabi/cards/GlobalCardTracker.java
+++ src/hanabi/cards/GlobalCardTracker.java
@@ -5,6 +5,22 @@ import javax.swing.JTable;
 public class GlobalCardTracker {
 	
 	JTable cards;
+
+	public GlobalCardTracker() {
+		super();
+		String[] columnNames = {"Blue", "Green", "Red", "White", "Yellow", "Multicolor"};
+		Integer[][] rowData = {
+				{3, 3, 3, 3, 3, 0},
+				{2, 2, 2, 2, 2, 0},
+				{2, 2, 2, 2, 2, 0},
+				{2, 2, 2, 2, 2, 0},
+				{1, 1, 1, 1, 1, 0}
+		};
+		//TODO this should update the number of multicolor cards depending on the variant
+		this.cards = new JTable(rowData, columnNames);
+	}
+	
+	
 	
 	//TODO eliminateCard(Card card);
 
