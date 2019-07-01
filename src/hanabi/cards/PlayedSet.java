03c48283ca3384497a85fa66e80e76cb47278c9b
Alexis Low <alexisthelow@gmail.com>
2019-07-01 09:46:17 -0600

    added playedSet to model cards thata have been successfully played

---
+++ src/hanabi/cards/PlayedSet.java
@@ -0,0 +1,13 @@
+package hanabi.cards;
+
+public class PlayedSet {
+	
+	private ArrayList<Card> blues = new ArrayList<Card>();
+	private ArrayList<Card> greens = new ArrayList<Card>();
+	private ArrayList<Card> reds = new ArrayList<Card>();
+	private ArrayList<Card> whites = new ArrayList<Card>();
+	private ArrayList<Card> yellows = new ArrayList<Card>();
+	private ArrayList<Card> multicolors = new ArrayList<Card>();
+	
+
+}
