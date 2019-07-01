88481da19901dfc3ad9fc80b62766ed800dd465b
Alexis Low <alexisthelow@gmail.com>
2019-07-01 16:08:03 -0600

    added variant bools to game

--- src/hanabi/game/Game.java
+++ src/hanabi/game/Game.java
@@ -2,7 +2,15 @@ package hanabi.game;
 
 import java.util.ArrayList;
 
+import hanabi.cards.Deck;
+
 public class Game {
 	
+	private Boolean variantOne = false;
+	private Boolean variantTwo = false;
+	private Boolean variantThree = false;
+	private Boolean variantFour = false;
 	private ArrayList<AbstractPlayer> players = new ArrayList<AbstractPlayer>();
+	private Deck deck;
+	
 }
