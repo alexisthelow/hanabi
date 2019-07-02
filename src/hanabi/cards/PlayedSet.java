3fa3dfa36eb60accfbd0a09ba9a81d71109b9914
Alexis Low <alexisthelow@gmail.com>
2019-07-02 11:27:35 -0600

    added to PlayedSet.playableCheck

--- src/hanabi/cards/PlayedSet.java
+++ src/hanabi/cards/PlayedSet.java
@@ -12,7 +12,7 @@ public class PlayedSet {
 	private ArrayList<Card> multicolors = new ArrayList<Card>();
 	
 	
-	public Boolean playableCheck(Card card) {
+	public Boolean playableCheck(Card card) throws Exception {
 		
 		ArrayList<Card> relevantColor;
 		
@@ -23,25 +23,35 @@ public class PlayedSet {
 				break;
 				
 			case GREEN:
-				relevantColor = 
+				relevantColor = greens;
 				break;
 				
 			case RED:
-				
+				relevantColor = reds;
 				break;
 				
 			case WHITE:
-				
+				relevantColor = whites;
 				break;
 				
 			case YELLOW:
-				
+				relevantColor = yellows;
 				break;
 				
 			case MULTICOLOR:
-				
+				relevantColor = multicolors;
+				break;
+			default:
+				throw new Exception("PlayedSet.playableCheck: couldn't find correct card array for card " + card.getColor() + card.getSuit().getNumeral());
 				break;
 			
+		}
+		
+		if(card.getSuit().getNumeral()==relevantColor.size() + 1) {
+			relevantColor.add(card);
+		}
+		else {
+			//TODO the check has failed, a fuse should be lost, and the card added to the discard pile
 		}
 	}
 	
