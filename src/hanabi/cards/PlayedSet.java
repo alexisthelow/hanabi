28b6be14d58ff9adb9386b24500764d3d320aa17
Alexis Low <alexisthelow@gmail.com>
2019-07-02 13:32:34 -0600

    added constructor and getters

--- src/hanabi/cards/PlayedSet.java
+++ src/hanabi/cards/PlayedSet.java
@@ -11,28 +11,38 @@ public class PlayedSet {
 	private ArrayList<Card> yellows = new ArrayList<Card>();
 	private ArrayList<Card> multicolors = new ArrayList<Card>();
 	
+	public PlayedSet() {
+		super();
+		this.blues = new ArrayList<Card>();
+		this.greens = new ArrayList<Card>();
+		this.reds = new ArrayList<Card>();
+		this.whites = new ArrayList<Card>();
+		this.yellows = new ArrayList<Card>();
+		this.multicolors = new ArrayList<Card>();
+	}
+
 	public ArrayList<Card> getBlues() {
 		return blues;
 	}
+
 	public ArrayList<Card> getGreens() {
 		return greens;
 	}
+
 	public ArrayList<Card> getReds() {
 		return reds;
 	}
+	
 	public ArrayList<Card> getWhites() {
 		return whites;
 	}
+	
 	public ArrayList<Card> getYellows() {
 		return yellows;
 	}
+	
 	public ArrayList<Card> getMulticolors() {
 		return multicolors;
 	}
-	
-	
-	
-	
-	
 
 }
