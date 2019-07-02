349f4c589bc7b53bcc877359b1fe56d0d663b3de
Alexis Low <alexisthelow@gmail.com>
2019-07-02 13:31:15 -0600

    added constructor and getters

--- src/hanabi/cards/DiscardPile.java
+++ src/hanabi/cards/DiscardPile.java
@@ -11,6 +11,38 @@ public class DiscardPile {
 	private ArrayList<Card> yellows;
 	private ArrayList<Card> multicolors;
 	
+	public DiscardPile() {
+		super();
+		this.blues = new ArrayList<Card>();
+		this.greens = new ArrayList<Card>();
+		this.reds = new ArrayList<Card>();
+		this.whites = new ArrayList<Card>();
+		this.yellows = new ArrayList<Card>();
+		this.multicolors = new ArrayList<Card>();
+	}
+	
+	public ArrayList<Card> getBlues() {
+		return blues;
+	}
+	
+	public ArrayList<Card> getGreens() {
+		return greens;
+	}
 
+	public ArrayList<Card> getReds() {
+		return reds;
+	}
+	
+	public ArrayList<Card> getWhites() {
+		return whites;
+	}
+	
+	public ArrayList<Card> getYellows() {
+		return yellows;
+	}
+	
+	public ArrayList<Card> getMulticolors() {
+		return multicolors;
+	}
 
 }
