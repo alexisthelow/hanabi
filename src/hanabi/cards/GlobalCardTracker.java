e37cf23313b339df22a172df062ed7c23c601546
Alexis Low <alexisthelow@gmail.com>
2019-07-03 17:26:19 -0600

    i'm about to switch from using a jtable to just using a 2d array like i should have in the first place

--- src/hanabi/cards/GlobalCardTracker.java
+++ src/hanabi/cards/GlobalCardTracker.java
@@ -6,18 +6,39 @@ public class GlobalCardTracker {
 	
 	JTable cards;
 
-	public GlobalCardTracker(Boolean variantActive) {
+	public GlobalCardTracker(int colorVariant) {
 		super();
 		String[] columnNames = {"Blue", "Green", "Red", "White", "Yellow", "Multicolor"};
-		Integer[][] rowData = {
-				{3, 3, 3, 3, 3, 0},
-				{2, 2, 2, 2, 2, 0},
-				{2, 2, 2, 2, 2, 0},
-				{2, 2, 2, 2, 2, 0},
-				{1, 1, 1, 1, 1, 0}
-		};
-		//TODO this should update the number of multicolor cards depending on the variant
-		this.cards = new JTable(rowData, columnNames);
+		if (colorVariant == 0 || colorVariant > 3) { // no color variant
+			Integer[][] rowData = {
+					{3, 3, 3, 3, 3, 0},
+					{2, 2, 2, 2, 2, 0},
+					{2, 2, 2, 2, 2, 0},
+					{2, 2, 2, 2, 2, 0},
+					{1, 1, 1, 1, 1, 0}
+			};
+			this.cards = new JTable(rowData, columnNames);
+		}
+		else if (colorVariant == 1 || colorVariant == 3) { // avalanche or wild
+			Integer[][] rowData = {
+					{3, 3, 3, 3, 3, 3},
+					{2, 2, 2, 2, 2, 3},
+					{2, 2, 2, 2, 2, 3},
+					{2, 2, 2, 2, 2, 3},
+					{1, 1, 1, 1, 1, 3}
+			};
+			this.cards = new JTable(rowData, columnNames);
+		}
+		else  { // avalanche single
+			Integer[][] rowData = {
+					{3, 3, 3, 3, 3, 1},
+					{2, 2, 2, 2, 2, 1},
+					{2, 2, 2, 2, 2, 1},
+					{2, 2, 2, 2, 2, 1},
+					{1, 1, 1, 1, 1, 1}
+			};
+			this.cards = new JTable(rowData, columnNames);
+		}
 	}
 	
 	
