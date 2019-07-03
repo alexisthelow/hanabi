819bb7a7bb486e8164c89a901d977908497df9c5
Alexis Low <alexisthelow@gmail.com>
2019-07-03 17:28:30 -0600

    added int value to Color

--- src/hanabi/cards/Color.java
+++ src/hanabi/cards/Color.java
@@ -2,18 +2,22 @@ package hanabi.cards;
 
 public enum Color {
 	
-	RED("Red"), GREEN("Green"), YELLOW("Yellow"), BLUE("Blue"), WHITE("White"), MULTICOLOR("Multicolor");
+	RED("Red", 3), GREEN("Green", 2), YELLOW("Yellow", 4), BLUE("Blue", 1), WHITE("White", 5), MULTICOLOR("Multicolor", 6);
 	
 	private String name;
+	private int value;
 
-	private Color(String name) {
+	private Color(String name, int value) {
 		this.name = name;
+		this.value = value;
 	}
 
 	public String getName() {
 		return name;
 	}
-	
-	
 
+	public int getValue() {
+		return value;
+	}
+	
 }
