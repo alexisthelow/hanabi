557aef2b90e947ba6f73c9e04786756c220cb199
Alexis Low <alexisthelow@gmail.com>
2019-07-08 09:56:27 -0600

    fixed constructor, added test

---
+++ Tests/GlobalCardTrackerTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import hanabi.cards.GlobalCardTracker;
+
+class GlobalCardTrackerTest {
+	
+	GlobalCardTracker gct;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		gct = new GlobalCardTracker(0);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		gct = null;
+	}
+
+	@Test
+	void test() {
+		System.out.println(gct.toString());
+	}
+
+}
--- src/hanabi/cards/GlobalCardTracker.java
+++ src/hanabi/cards/GlobalCardTracker.java
@@ -21,42 +21,16 @@ public class GlobalCardTracker {
 				}
 				else { //fives
 					for (int j = 0; j < cards[i].length - 1; j++) {
-						cards[i][j] = 3;
+						cards[i][j] = 1;
 					}
 				}
 			}
+			cards[0][5] = 0;
+			cards[1][5] = 0;
+			cards[2][5] = 0;
+			cards[3][5] = 0;
+			cards[4][5] = 0;
 			
-			//ones
-			cards[0][0] = 3;
-			cards[0][1] = 3;
-			cards[0][2] = 3;
-			cards[0][3] = 3;
-			cards[0][4] = 3;
-			
-			//twos
-			cards[1][0] = 2;
-			cards[1][1] = 2;
-			cards[1][2] = 2;
-			cards[1][3] = 2;
-			cards[1][4] = 2;
-			
-			//threes
-			cards[2][0] = 2;
-			cards[2][1] = 2;
-			cards[2][2] = 2;
-			cards[2][3] = 2;
-			cards[2][4] = 2;
-			
-			//fours
-			cards[3][0] = 2;
-			cards[3][1] = 2;
-			cards[3][2] = 2;
-			cards[3][3] = 2;
-			cards[3][4] = 2;
-			
-			//fives
-
-
 		}
 		else if (colorVariant == 1 || colorVariant == 3) { // avalanche or wild
 
@@ -71,8 +45,8 @@ public class GlobalCardTracker {
 	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("    | Blue | Green | Red | White | Yellow | Multicolor |\n")
-		.append("Ones: " + cards[0][0] + "  " + cards[0][1] + "  " + cards[0][2] + "  " + cards[0][3] + "  " + cards[0][4] + "  " + cards[0][5] + "\n")
+		sb.append("       | Blue | Green | Red | White | Yellow | Multicolor |\n")
+		.append("Ones:    " + cards[0][0] + "  " + cards[0][1] + "  " + cards[0][2] + "  " + cards[0][3] + "  " + cards[0][4] + "  " + cards[0][5] + "\n")
 		.append("Twos: " + cards[1][0] + "  " + cards[1][1] + "  " + cards[1][2] + "  " + cards[1][3] + "  " + cards[1][4] + "  " + cards[1][5] + "\n")
 		.append("Threes: " + cards[2][0] + "  " + cards[2][1] + "  " + cards[2][2] + "  " + cards[2][3] + "  " + cards[2][4] + "  " + cards[2][5] + "\n")
 		.append("Fours: " + cards[3][0] + "  " + cards[3][1] + "  " + cards[3][2] + "  " + cards[3][3] + "  " + cards[3][4] + "  " + cards[3][5] + "\n")
