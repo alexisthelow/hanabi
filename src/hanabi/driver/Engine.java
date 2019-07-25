c4da709aff5b20b00fb8b406cb7e9e5ef5002b51
Alexis Low <alexisthelow@gmail.com>
2019-07-25 16:07:10 -0600

    outline of menus

--- src/hanabi/driver/Engine.java
+++ src/hanabi/driver/Engine.java
@@ -1,13 +1,11 @@
 package hanabi.driver;
 
-import java.util.Scanner;
-
 import hanabi.game.Game;
 
 public class Engine {
 
 	//objects
-	Scanner kb = new Scanner(System.in);
+	private static ScannerResponseGetter srg = new ScannerResponseGetter();
 	
 	private Game game;
 	
@@ -19,7 +17,6 @@ public class Engine {
 		//create new game
 		//select variants
 		//add players
-		//discover first player
 		//proceed with play
 			//player cannot discard if clocks == 8; player cannot provide info if clocks == 0
 		//stop one round after cards are gone OR if all fuses are burned OR if grandFinale == true, stop on losing necessary card OR stop when all colors are built to maximum
@@ -28,8 +25,30 @@ public class Engine {
 		
 	}
 	
+	//main menu
+	public int mainMenu() {
+		System.out.println("~Hanabi~");
+		System.out.println("--------");
+		System.out.println("1) Start new game");
+		System.out.println("2) Exit");
+		
+		//TODO add logic for selection
+		
+	}
+	
+	//start new game menu
+	public int newGameMenu() {
+		System.out.println("New Game");
+		System.out.println("--------");
+		System.out.println("1) Add player");
+		System.out.println("2) Select variants");
+		System.out.println("3) Start game");
+		System.out.println("4) Exit");
+		
+	}
+	
 	//select variants
-	public void selectVariant(Scanner kb) {
+	public void selectVariant() {
 		
 	}
 	
